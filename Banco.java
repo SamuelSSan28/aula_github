3a59d6ee637f6f0dde2e5ba441cc3b615a1f688a
giselesousar <giselesousar@gmail.com>
2021-05-07 17:15:58 -0300

    criando metodo abrirConta e adicionado opcao no menu #2

--- Banco.java
+++ Banco.java
@@ -16,9 +16,33 @@ public class Banco {
         return "O cliente já existe.";
     }
 
+    public String abrirConta(String cpf) {
+        if(!this.verificaExisteCliente(cpf)){
+            return "Cliente não existe!";
+        }
+        
+        if(this.verificaExisteConta(cpf)){
+            return "O cliente já possui conta";
+        }
+
+        int numero = contas.size() + 1;
+        Conta conta = new Conta(cpf, numero);
+        contas.add(conta);
+        return "Conta aberta com sucesso. O número da conta é: " + numero;
+    }
+
     private Boolean verificaExisteCliente(String cpf) {
         for (Cliente cliente : clientes) {
             if(cliente.getCpf().equals(cpf)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Boolean verificaExisteConta(String cpf) {
+        for (Conta conta : contas) {
+            if(conta.getCpf().equals(cpf)) {
                 return true;
             }
         }
